bc5bc9ba6f019b1ac234f5ea008ee8c3556daa55
Rene Fuehrer <rene.fuehrer@generali.com>
2020-07-04 08:37:58 +0200

    Add filter for last n days in graph Add configuration option (via settings.js) Change static text to configurable settings Fix display of milestones out of range Fix typos

---
+++ backend/settings.js
@@ -0,0 +1,24 @@
+var settings = {
+    team: '263581', 
+    teamname: 'Red Insurance Lions ("Red Lions")',
+    limitDays: 14,
+    milestone1: 10000,
+    milestone2: 5000,
+    milestone3: 1000,
+    goal: 150
+}
+
+var language = {
+    progress:{
+      title: "Verlauf",
+      help: "Der Verlauf stellt die grafische Auswertung aller Veränderung im Team-Ranking dar. Die Anzeige umfasst  die Entwicklung der vergangenen Tage (siehe Graph). Über die Schaltfläche kann die Grafik als Bilddatei gesichert werden."
+    },
+    history:{
+      title: "Historische Werte",
+      help: "Die historischen Werte stellen alle Änderungen des Rankings in einer tabellarischen Übersicht dar. Die Prüfung erfolgt in der Regel jede Minute. Es werden jedoch nur Einträge in dieser Übersicht vorgenommen, wenn eine Änderung im Ranking festgestellt wurde."
+    },
+    supporter:{
+      title: "Unterstützer",
+      help: "Die Übersicht stellt die für eine Änderung im Ranking beisteuernden IDs dar. Es werden hier aktive Team-Mitglieder mit einem eigenen Rang aufgeführt. Die genannten Mitwirkdenden haben während der letzten Veränderung des Team-Rankings sogenannte 'Credits' für das Team errechnet und damit zur Veränderung der Platzierung beigetragen. Bei negativen Veränderungen des Team-Rankings haben diese Mitglieder aktiv dabei unterstützt, dass die Veränderung im Sinne des Teams geringer ausfiel."
+    }
+  }
